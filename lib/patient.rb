669a58088c6908aad8d6b0a03861cb3bc6b865b3
Galin McMahon and Susha Dore <galin@>
2017-05-02 13:23:35 -0700

    pass #birthday spec

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,11 +1,12 @@
 class Patient
-  attr_reader(:patient_name, :doctor_id, :id)
+  attr_reader(:patient_name, :doctor_id, :id, :birthday)
   @@all_patients = []
 
   define_method(:initialize) do |attributes|
     @patient_name = attributes.fetch(:patient_name)
     @doctor_id = attributes.fetch(:doctor_id)
     @id = attributes.fetch(:id)
+    @birthday = attributes.fetch(:birthday)
   end
 
   define_singleton_method(:all) do
@@ -14,13 +15,13 @@ class Patient
     returned_patients.each() do |patient|
       patient_name = patient.fetch("patient_name")
       doctor_id = patient.fetch("doctor_id").to_i() # The information comes out of the database as a string.
-      patients.push(Patient.new({:patient_name => patient_name, :doctor_id => doctor_id, :id => nil}))
+      patients.push(Patient.new({:patient_name => patient_name, :doctor_id => doctor_id, :id => nil, :birthday => "1975-01-01"}))
     end
     patients
   end
 #
     define_method(:save) do
-      DB.exec("INSERT INTO patients (patient_name, doctor_id) VALUES ('#{@patient_name}', #{@doctor_id});")
+      DB.exec("INSERT INTO patients (patient_name, doctor_id, birthday) VALUES ('#{@patient_name}', #{@doctor_id}, '#{@birthday}');")
     end
 #
   define_method(:==) do |another_patient|
--- spec/patient_spec.rb
+++ spec/patient_spec.rb
@@ -12,22 +12,29 @@ end
 
 describe("#patient_name") do
   it("lets you read the patient_name out") do
-    test_patient = Patient.new({:patient_name => "Jones", :doctor_id => 1, :id =>nil})
+    test_patient = Patient.new({:patient_name => "Jones", :doctor_id => 1, :id => nil, :birthday => "1975-01-01"})
     expect(test_patient.patient_name()).to(eq("Jones"))
   end
 end
 
 describe("#doctor_id") do
   it("lets you read the doctor ID out") do
-    test_patient = Patient.new({:patient_name => "Jones", :doctor_id => 1, :id =>nil})
+    test_patient = Patient.new({:patient_name => "Jones", :doctor_id => 1, :id => nil, :birthday => "1975-01-01"})
     expect(test_patient.doctor_id()).to(eq(1))
   end
 end
 
 describe("#id") do
   it("lets you read the patient ID out") do
-    test_patient = Patient.new({:patient_name => "Jones", :doctor_id => 1, :id => nil})
+    test_patient = Patient.new({:patient_name => "Jones", :doctor_id => 1, :id => nil, :birthday => "1975-01-01", :birthday => "1975-01-01"})
     expect(test_patient.id()).to(eq(nil))
+  end
+end
+
+describe("#birthday") do
+  it("lets you read the patient birthday") do
+    test_patient = Patient.new({:patient_name => "Jones", :doctor_id => 1, :id => nil, :birthday => "1975-01-01"})
+    expect(test_patient.birthday()).to(eq("1975-01-01"))
   end
 end
 
@@ -39,15 +46,15 @@ end
   end
   describe("#==") do
     it("is the same patient if it has the same patient_name and doctor ID") do
-      patient1 = Patient.new({:patient_name => "Jones", :doctor_id => 1, :id => nil})
-      patient2 = Patient.new({:patient_name => "Jones", :doctor_id => 1, :id => nil})
+      patient1 = Patient.new({:patient_name => "Jones", :doctor_id => 1, :id => nil, :birthday => "1975-01-01"})
+      patient2 = Patient.new({:patient_name => "Jones", :doctor_id => 1, :id => nil, :birthday => "1975-01-01"})
       expect(patient1).to(eq(patient2))
     end
   end
 #
   describe("#save") do
     it("adds a patient to the array of saved patients") do
-      test_patient = Patient.new({:patient_name => "Jones", :doctor_id => 1, :id => nil})
+      test_patient = Patient.new({:patient_name => "Jones", :doctor_id => 1, :id => nil, :birthday => "1975-01-01"})
       test_patient.save()
       expect(Patient.all()).to(eq([test_patient]))
     end
