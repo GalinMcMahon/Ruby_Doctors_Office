0c934251dcbf33091d7c958027ec7e604f04819e
Galin McMahon and Susha Dore <susha@>
2017-05-02 11:48:21 -0700

    pass .all spec

--- lib/patient.rb
+++ lib/patient.rb
@@ -9,17 +9,17 @@ class Patient
       @doctor_id = attributes.fetch(:doctor_id)
       @id = attributes.fetch(:id)
     end
-#
-#     define_singleton_method(:all) do
-#       returned_patients = DB.exec("SELECT * FROM patients;")
-#       patients = []
-#       returned_patients.each() do |patient|
-#         patient_name = patient.fetch("patient_name")
-#         doctor_id = patient.fetch("doctor_id").to_i() # The information comes out of the database as a string.
-#         patients.push(patient.new({:patient_name => patient_name, :doctor_id => doctor_id}))
-#       end
-#       patients
-#     end
+
+    define_singleton_method(:all) do
+      returned_patients = DB.exec("SELECT * FROM patients;")
+      patients = []
+      returned_patients.each() do |patient|
+        patient_name = patient.fetch("patient_name")
+        doctor_id = patient.fetch("doctor_id").to_i() # The information comes out of the database as a string.
+        patients.push(patient.new({:patient_name => patient_name, :doctor_id => doctor_id}))
+      end
+      patients
+    end
 #
 #     define_method(:save) do
 #       DB.exec("INSERT INTO patients (patient_name, doctor_id) VALUES ('#{@patient_name}', #{@doctor_id});")
